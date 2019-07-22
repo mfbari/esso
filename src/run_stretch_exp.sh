a75bc50acc8806cddc0b4a3d28cb39d180555fbc
mfbari <faizulbari@gmail.com>
2019-07-22 13:42:49 -0400

    add bash script to run parallel experiments

---
+++ src/run_stretch_exp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..10}; do
+  for j in {3600..3600..1800}; do
+    arrival_rate=$( echo $i*0.01 | bc);
+    algo='c'
+    dataset=$(printf '13129_%4.2f_%d' "$arrival_rate" "$j")
+    eval 'python run_simulation.py -'$algo'ri'$dataset'_2 ../data/'$dataset'/ &> '$algo'_running_time_'$dataset'.dat &'
+    echo $!
+  done
+done
