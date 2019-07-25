ab7d7f1787805283102f13848069fd3499dde14a
mfbari <faizulbari@gmail.com>
2019-07-25 19:25:39 -0400

    update run code for cplex

--- src/run_stretch_exp.sh
+++ src/run_stretch_exp.sh
@@ -3,9 +3,9 @@
 for i in {1..10}; do
   for j in {3600..3600..1800}; do
     arrival_rate=$( echo $i*0.01 | bc);
-    algo='f'
+    algo='c'
     dataset=$(printf '13129_%4.2f_%d' "$arrival_rate" "$j")
-    eval 'python run_simulation.py -'$algo'ri'$dataset'_2 ../data/'$dataset'/ &> '$algo'_running_time_'$dataset'.dat &'
+    eval 'python run_simulation.py -'$algo'ri'$dataset'_3 ../data/'$dataset'/ &> '$algo'_running_time_'$dataset'.dat &'
     echo $!
   done
 done
