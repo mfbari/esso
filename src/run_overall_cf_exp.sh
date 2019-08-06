616a93242a8900d1852bb8d21cbad91122f10393
mfbari <faizulbari@gmail.com>
2019-08-06 16:48:21 -0400

    update params in run_overall_carbon_fp script.

--- src/run_overall_cf_exp.sh
+++ src/run_overall_cf_exp.sh
@@ -2,7 +2,7 @@
 
 for s in {1..10}; do
   arr_rate=$( echo $s*0.01 | bc);
-  algo='c'
+  algo='f'
   dataset=$(printf '13129_%4.2f_3600' "$arr_rate")
   eval 'python run_simulation.py -'$algo'ri'$dataset' ../data/'$dataset'/ &> '$algo'_ocf_output_'$dataset'.dat &'
   echo $!
